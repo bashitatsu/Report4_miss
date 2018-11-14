83fa6163a58e44d2e2fe12258a4570c7e79af48a
Tatsuya Ishibashi <e185714@ie.u-ryukyu.ac.jp>
2018-11-14 09:00:20 +0900

    2nd commit

---
+++ src/jp/ac/uryukyu/ie/e185714/Exception.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e185714;
+
+public class Exception {
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+    }
+}
